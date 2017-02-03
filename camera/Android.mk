e5d4d1af703c73a4bf5a4123a7844fc3ad1829f4
papjul <julien.papasian@gmail.com>
2017-02-03 22:29:56 +0100

    Add modifications from device mako

--- camera/Android.mk
+++ camera/Android.mk
@@ -93,11 +93,14 @@ ifeq ($(USE_DEVICE_SPECIFIC_CAMERA),true)
         LOCAL_C_INCLUDES+= $(LOCAL_PATH)/mm-camera-interface
       endif
 
-      LOCAL_C_INCLUDES+= hardware/qcom/display/msm8960/libgralloc
-      LOCAL_C_INCLUDES+= hardware/qcom/display/msm8960/libgenlock
+      LOCAL_C_INCLUDES+= $(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libgralloc
+      LOCAL_C_INCLUDES+= $(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libgenlock
       LOCAL_C_INCLUDES+= frameworks/native/include/media/hardware
-      LOCAL_C_INCLUDES+= hardware/qcom/media/msm8974/libstagefrighthw
+      LOCAL_C_INCLUDES+= $(call project-path-for,qcom-media)/libstagefrighthw
 
+      LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+      LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
+      LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
       ifeq ($(V4L2_BASED_LIBCAM),true)
         LOCAL_SHARED_LIBRARIES:= libutils libui libcamera_client libcamera_metadata liblog libcutils
