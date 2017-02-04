1c73be7f52a0423e700a0b2581a2da8ad51a69d6
papjul <julien.papasian@gmail.com>
2017-02-04 13:16:29 +0100

    Revert to HAL audio

--- libaudioamp/Android.mk
+++ libaudioamp/Android.mk
@@ -17,17 +17,14 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 # audio HAL includes
-#LOCAL_C_INCLUDES += \
-#	external/tinyalsa/include \
-#	external/tinycompress/include \
-#	$(call include-path-for, audio-route) \
-#	$(call include-path-for, audio-effects) \
-#	$(call project-path-for,qcom-audio)/hal \
-#	$(call project-path-for,qcom-audio)/hal/$(TARGET_BOARD_PLATFORM) \
-#	$(call project-path-for,qcom-audio)/hal/audio_extn
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_C_INCLUDES += hardware/qcom/audio-caf/msm8960/alsa_sound
-LOCAL_C_INCLUDES += hardware/qcom/audio-caf/msm8960/libalsa-intf
+LOCAL_C_INCLUDES += \
+	external/tinyalsa/include \
+	external/tinycompress/include \
+	$(call include-path-for, audio-route) \
+	$(call include-path-for, audio-effects) \
+	$(call project-path-for,qcom-audio)/hal \
+	$(call project-path-for,qcom-audio)/hal/$(TARGET_BOARD_PLATFORM) \
+	$(call project-path-for,qcom-audio)/hal/audio_extn
 
 ifeq ($(strip $(BOARD_USES_QCOM_HARDWARE)),true)
     LOCAL_CFLAGS += -DAUDIO_CAF
