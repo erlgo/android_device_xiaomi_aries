5537e3108b0ec17406050a18a9aef8f727e543f6
papjul <julien.papasian@gmail.com>
2017-02-08 23:35:09 +0100

    audio: Fix compilation issues

--- amplifier/audio_amplifier.c
+++ amplifier/audio_amplifier.c
@@ -54,142 +54,6 @@ static audio_mode_t mMode = AUDIO_MODE_NORMAL;
 static uint32_t in_snd_device = SND_DEVICE_NONE;
 static uint32_t out_snd_device = SND_DEVICE_NONE;
 
-static int amp_set_mode(amplifier_device_t *device, audio_mode_t mode)
-{
-	int ret = 0;
-	aries_device_t *dev = (aries_device_t *) device;
-
-	if ((mode < AUDIO_MODE_CURRENT) || (mode >= AUDIO_MODE_CNT)) {
-		ALOGW("%s: invalid mode=%d\n", __func__, mode);
-		ret = -1;
-	} else {
-		if (mMode != mode || dev->current_mode != mode) {
-			ALOGD("%s: mode: %d->%d\n", __func__, dev->current_mode, mode);
-			mMode = mode;
-			dev->current_mode = mode;
-			es310_do_route();
-		}
-	}
-
-	return ret;
-}
-
-static int amp_set_input_devices(amplifier_device_t *device, uint32_t devices)
-{
-	uint32_t new_in_snd_device = SND_DEVICE_NONE;
-	uint32_t new_out_snd_device = SND_DEVICE_NONE;
-
-	if (devices != 0) {
-		if (devices >= SND_DEVICE_OUT_BEGIN
-		    && devices < SND_DEVICE_OUT_END)
-			new_out_snd_device = devices;
-		else
-			new_in_snd_device = devices;
-
-		if (new_in_snd_device != in_snd_device
-		    || new_out_snd_device != out_snd_device) {
-			in_snd_device = new_in_snd_device;
-			out_snd_device = new_out_snd_device;
-
-			es310_do_route();
-		}
-	}
-
-	return 0;
-}
-
-static int amp_set_output_devices(amplifier_device_t *device, uint32_t devices)
-{
-	uint32_t new_in_snd_device = SND_DEVICE_NONE;
-	uint32_t new_out_snd_device = SND_DEVICE_NONE;
-
-	if (devices != 0) {
-		if (devices >= SND_DEVICE_OUT_BEGIN
-		    && devices < SND_DEVICE_OUT_END)
-			new_out_snd_device = devices;
-		else
-			new_in_snd_device = devices;
-
-		if (new_in_snd_device != in_snd_device
-		    || new_out_snd_device != out_snd_device) {
-			in_snd_device = new_in_snd_device;
-			out_snd_device = new_out_snd_device;
-
-			es310_do_route();
-		}
-	}
-
-	return 0;
-}
-
-static int amp_dev_close(hw_device_t *device)
-{
-	close(mES310Fd);
-	mES310Fd = -1;
-	aries_device_t *dev = (aries_device_t *) device;
-
-	free(dev);
-
-	return 0;
-}
-
-static int amp_module_open(const hw_module_t *module, UNUSED const char *name,
-        hw_device_t **device)
-{
-	if (aries_dev) {
-		ALOGE("%s:%d: Unable to open second instance of ES310 amplifier\n",
-			__func__, __LINE__);
-		return -EBUSY;
-	}
-
-	aries_dev = calloc(1, sizeof(aries_device_t));
-	if (!aries_dev) {
-		ALOGE("%s:%d: Unable to allocate memory for amplifier device\n",
-			__func__, __LINE__);
-		return -ENOMEM;
-	}
-
-	aries_dev->amp_dev.common.tag = HARDWARE_DEVICE_TAG;
-	aries_dev->amp_dev.common.module = (hw_module_t *) module;
-	aries_dev->amp_dev.common.version = HARDWARE_DEVICE_API_VERSION(1, 0);
-	aries_dev->amp_dev.common.close = amp_dev_close;
-
-	aries_dev->amp_dev.set_input_devices = amp_set_input_devices;
-	aries_dev->amp_dev.set_output_devices = amp_set_output_devices;
-	aries_dev->amp_dev.enable_input_devices = NULL;
-	aries_dev->amp_dev.enable_output_devices = NULL;
-	aries_dev->amp_dev.set_mode = amp_set_mode;
-	aries_dev->amp_dev.output_stream_start = NULL;
-	aries_dev->amp_dev.input_stream_start = NULL;
-	aries_dev->amp_dev.output_stream_standby = NULL;
-	aries_dev->amp_dev.input_stream_standby = NULL;
-
-	aries_dev->current_mode = AUDIO_MODE_NORMAL;
-
-	*device = (hw_device_t *) aries_dev;
-
-        // mutex init
-	pthread_mutex_init(&mES310Mutex, NULL);
-
-	return es310_init();
-}
-
-static struct hw_module_methods_t hal_module_methods = {
-    .open = amp_module_open,
-};
-
-amplifier_module_t HAL_MODULE_INFO_SYM = {
-    .common = {
-        .tag = HARDWARE_MODULE_TAG,
-        .module_api_version = AMPLIFIER_MODULE_API_VERSION_0_1,
-        .hal_api_version = HARDWARE_HAL_API_VERSION,
-        .id = AMPLIFIER_HARDWARE_MODULE_ID,
-        .name = "MI2 audio amplifier HAL",
-        .author = "The CyanogenMod Open Source Project",
-        .methods = &hal_module_methods,
-    },
-};
-
 static const char *es310_getNameByPresetID(int presetID)
 {
 	switch (presetID) {
@@ -562,3 +426,139 @@ unlock:
 	pthread_mutex_unlock(&mES310Mutex);
 	return rc;
 }
+
+static int amp_set_mode(amplifier_device_t *device, audio_mode_t mode)
+{
+	int ret = 0;
+	aries_device_t *dev = (aries_device_t *) device;
+
+	if ((mode < AUDIO_MODE_CURRENT) || (mode >= AUDIO_MODE_CNT)) {
+		ALOGW("%s: invalid mode=%d\n", __func__, mode);
+		ret = -1;
+	} else {
+		if (mMode != mode || dev->current_mode != mode) {
+			ALOGD("%s: mode: %d->%d\n", __func__, dev->current_mode, mode);
+			mMode = mode;
+			dev->current_mode = mode;
+			es310_do_route();
+		}
+	}
+
+	return ret;
+}
+
+static int amp_set_input_devices(amplifier_device_t *device, uint32_t devices)
+{
+	uint32_t new_in_snd_device = SND_DEVICE_NONE;
+	uint32_t new_out_snd_device = SND_DEVICE_NONE;
+
+	if (devices != 0) {
+		if (devices >= SND_DEVICE_OUT_BEGIN
+		    && devices < SND_DEVICE_OUT_END)
+			new_out_snd_device = devices;
+		else
+			new_in_snd_device = devices;
+
+		if (new_in_snd_device != in_snd_device
+		    || new_out_snd_device != out_snd_device) {
+			in_snd_device = new_in_snd_device;
+			out_snd_device = new_out_snd_device;
+
+			es310_do_route();
+		}
+	}
+
+	return 0;
+}
+
+static int amp_set_output_devices(amplifier_device_t *device, uint32_t devices)
+{
+	uint32_t new_in_snd_device = SND_DEVICE_NONE;
+	uint32_t new_out_snd_device = SND_DEVICE_NONE;
+
+	if (devices != 0) {
+		if (devices >= SND_DEVICE_OUT_BEGIN
+		    && devices < SND_DEVICE_OUT_END)
+			new_out_snd_device = devices;
+		else
+			new_in_snd_device = devices;
+
+		if (new_in_snd_device != in_snd_device
+		    || new_out_snd_device != out_snd_device) {
+			in_snd_device = new_in_snd_device;
+			out_snd_device = new_out_snd_device;
+
+			es310_do_route();
+		}
+	}
+
+	return 0;
+}
+
+static int amp_dev_close(hw_device_t *device)
+{
+	close(mES310Fd);
+	mES310Fd = -1;
+	aries_device_t *dev = (aries_device_t *) device;
+
+	free(dev);
+
+	return 0;
+}
+
+static int amp_module_open(const hw_module_t *module, UNUSED const char *name,
+        hw_device_t **device)
+{
+	if (aries_dev) {
+		ALOGE("%s:%d: Unable to open second instance of ES310 amplifier\n",
+			__func__, __LINE__);
+		return -EBUSY;
+	}
+
+	aries_dev = calloc(1, sizeof(aries_device_t));
+	if (!aries_dev) {
+		ALOGE("%s:%d: Unable to allocate memory for amplifier device\n",
+			__func__, __LINE__);
+		return -ENOMEM;
+	}
+
+	aries_dev->amp_dev.common.tag = HARDWARE_DEVICE_TAG;
+	aries_dev->amp_dev.common.module = (hw_module_t *) module;
+	aries_dev->amp_dev.common.version = HARDWARE_DEVICE_API_VERSION(1, 0);
+	aries_dev->amp_dev.common.close = amp_dev_close;
+
+	aries_dev->amp_dev.set_input_devices = amp_set_input_devices;
+	aries_dev->amp_dev.set_output_devices = amp_set_output_devices;
+	aries_dev->amp_dev.enable_input_devices = NULL;
+	aries_dev->amp_dev.enable_output_devices = NULL;
+	aries_dev->amp_dev.set_mode = amp_set_mode;
+	aries_dev->amp_dev.output_stream_start = NULL;
+	aries_dev->amp_dev.input_stream_start = NULL;
+	aries_dev->amp_dev.output_stream_standby = NULL;
+	aries_dev->amp_dev.input_stream_standby = NULL;
+
+	aries_dev->current_mode = AUDIO_MODE_NORMAL;
+
+	*device = (hw_device_t *) aries_dev;
+
+        // mutex init
+	pthread_mutex_init(&mES310Mutex, NULL);
+
+	return es310_init();
+}
+
+static struct hw_module_methods_t hal_module_methods = {
+    .open = amp_module_open,
+};
+
+amplifier_module_t HAL_MODULE_INFO_SYM = {
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .module_api_version = AMPLIFIER_MODULE_API_VERSION_0_1,
+        .hal_api_version = HARDWARE_HAL_API_VERSION,
+        .id = AMPLIFIER_HARDWARE_MODULE_ID,
+        .name = "MI2 audio amplifier HAL",
+        .author = "The CyanogenMod Open Source Project",
+        .methods = &hal_module_methods,
+    },
+};
